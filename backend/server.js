7882aa77d5fe6204bf4da94e16087c644f96195f
imyogeshgaikwad <imyogeshgaikwad@gmail.com>
2025-07-31 00:04:29 +0530

    Configured server file and environment variables

---
+++ backend/server.js
@@ -0,0 +1,55 @@
+import express from "express";
+import cors from "cors";
+import { connectDB } from "./config/db.js";
+import foodRouter from "./routes/foodRoute.js";
+import userRouter from "./routes/userRoute.js";
+import "dotenv/config";
+import cartRouter from "./routes/cartRoute.js";
+import orderRouter from "./routes/orderRoute.js";
+
+// app config
+const app = express();
+const port =process.env.PORT || 4000;
+
+//middlewares
+app.use(express.json());
+// app.use(cors());
+
+const allowedOrigins = [
+  process.env.FRONTEND_URL,
+  process.env.ADMIN_URL,
+];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("CORS Not Allowed"));
+    }
+  },
+  credentials: true
+}));
+
+// app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
+// app.use(cors({ origin: 'http://localhost:5174', credentials: true }));
+
+
+
+// DB connection
+connectDB();
+
+// api endpoints
+app.use("/api/food", foodRouter);
+app.use("/images", express.static("uploads"));
+app.use("/api/user", userRouter);
+app.use("/api/cart", cartRouter);
+app.use("/api/order", orderRouter);
+
+app.get("/", (req, res) => {
+  res.send("API Working");
+});
+
+app.listen(port, () => {
+  console.log(`Server Started on port: ${port}`);
+});
